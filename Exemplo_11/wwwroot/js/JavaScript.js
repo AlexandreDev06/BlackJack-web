2abb50af890c09497e082704008eca22c9a1452a
alexandre <alexandre.macedo1712@gmail.com>
2021-05-26 20:00:09 -0300

    Primeira versão

---
+++ Exemplo_11/wwwroot/js/JavaScript.js
@@ -0,0 +1,45 @@
+﻿var btn_jogador1 = document.getElementById("btn_jogador_1");
+var btn_jogador2 = document.getElementById("btn_jogador_2");
+var btn_parar1 = document.getElementById("btn_parar_1");
+var btn_parar2 = document.getElementById("btn_parar_2");
+
+function valorAleatorio() {
+    min = Math.ceil(1);
+    max = Math.floor(13);
+    var aleatorio = Math.floor(Math.random() * (max - min)) + min;
+    return aleatorio;
+}
+
+function jogador1() {
+    var cartas1 = document.getElementById("cartas1");
+    $(cartas1).attr("src", '/images/' + valorAleatorio() + '.png');
+}
+
+function jogador2() {
+    var cartas2 = document.getElementById("cartas2");
+    $(cartas2).attr("src", '/images/' + valorAleatorio() + '.png');
+}
+
+function parar1() {
+    $(btn_jogador1).attr('disabled', 'disabled');
+    $(btn_parar1).attr('disabled', 'disabled');
+
+    $(btn_jogador2).removeAttr('disabled');
+    $(btn_parar2).removeAttr('disabled');
+}
+
+function parar2() {
+    $(btn_jogador2).attr('disabled', 'disabled');
+    $(btn_parar2).attr('disabled', 'disabled');
+}
+
+function reiniciar() {
+    $(btn_jogador2).removeAttr('disabled');
+    $(btn_parar2).removeAttr('disabled');
+    $(btn_jogador1).removeAttr('disabled');
+    $(btn_parar1).removeAttr('disabled');
+}
+
+function iniciar() {
+
+}
