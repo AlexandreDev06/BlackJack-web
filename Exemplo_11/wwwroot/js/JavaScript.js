db279fc4c44eb1e838a850160a54ee7fff08efd1
alexandre <alexandre.macedo1712@gmail.com>
2021-05-26 22:02:48 -0300

    Versão completa

--- Exemplo_11/wwwroot/js/JavaScript.js
+++ Exemplo_11/wwwroot/js/JavaScript.js
@@ -9,9 +9,14 @@ var cartas2 = document.getElementById("cartas2");
 var resultado1 = document.getElementById('resultado_jogador1');
 var resultado2 = document.getElementById('resultado_jogador2');
 var resultado_total = document.getElementById('resultado_total');
+var jog1 = document.getElementById('nome1');
+var jog2 = document.getElementById('nome2');
 
+$(btn_jogador1).attr('disabled', 'disabled');
+$(btn_parar1).attr('disabled', 'disabled');
+$(btn_jogador2).attr('disabled', 'disabled');
+$(btn_parar2).attr('disabled', 'disabled');
 
-resultado_total
 function valorAleatorio() {
     min = Math.ceil(1);
     max = Math.floor(13);
@@ -22,38 +27,31 @@ function valorAleatorio() {
 function jogador1() {
     var aleatorio = valorAleatorio();
     $(cartas1).attr("src", '/images/' + aleatorio + '.png');
-
     resultado1.innerHTML = player1 += aleatorio;
 
-    //logica de vitoria do game------------------------
-    if (player1 > player2 && player1 <= 21) {
-        Resultado.Text = "Jogador 1 Ganhou";
+    if (player1 > 21) {
+        resultado_total.innerHTML = "Jogador 1 Estorou";
         $(btn_jogador1).attr('disabled', 'disabled');
         $(btn_parar1).attr('disabled', 'disabled');
-    }
-    else if (player1 < player2 && player2 <= 21) {
-        resultado_total.innerHTML ="Jogador 2 Ganhou";
-    }
-    else if (player1 > 21 && player2 <= 21) {
-        resultado_total.innerHTML =  "Jogador 2 Ganhou";
-    }
-    else if (player1 <= 21 && player2 <= 21) {
-        resultado_total.innerHTML = "Nenhum Ganhador!";
-    }
-    else if (player2 > 21 && player1 > 21) {
-        resultado_total.innerHTML = "A casa ganhou!";
-    }
-    else if (player2 > 21) {
-        resultado_total.innerHTML = "Jogador 1 Ganhou";
-    }
-    ///////////////------------------------------------------
+    } 
 }
 
 function jogador2() {
     var aleatorio = valorAleatorio();
     $(cartas2).attr("src", '/images/' + aleatorio + '.png');
-
     resultado2.innerHTML = player2 += aleatorio;
+
+    if (player1 < player2 && player2 <= 21) {
+        resultado_total.innerHTML = "Jogador 2 Ganhou";
+    } else if (player1 > 21 && player2 <= 21) {
+        resultado_total.innerHTML = "Jogador 2 Ganhou";
+    } else if (player2 > 21 && player1 > 21) {
+        resultado_total.innerHTML = "A casa ganhou!";
+    } else if (player2 > 21) {
+        resultado_total.innerHTML = "Jogador 1 Ganhou";
+        $(btn_jogador2).attr('disabled', 'disabled');
+        $(btn_parar2).attr('disabled', 'disabled');
+    }
 }
 
 function parar1() {
@@ -67,6 +65,10 @@ function parar1() {
 function parar2() {
     $(btn_jogador2).attr('disabled', 'disabled');
     $(btn_parar2).attr('disabled', 'disabled');
+
+    if (player1 == player2) {
+        resultado_total.innerHTML = "Empate!";
+    }
 }
 
 function reiniciar() {
@@ -82,8 +84,25 @@ function reiniciar() {
 
     resultado_total.innerHTML = "";
 
+    $(cartas1).attr("src", '/images/0.png');
+    $(cartas2).attr("src", '/images/0.png');
 }
 
 function iniciar() {
+    $(btn_jogador2).removeAttr('disabled');
+    $(btn_parar2).removeAttr('disabled');
+    $(btn_jogador1).removeAttr('disabled');
+    $(btn_parar1).removeAttr('disabled');
+}
 
+function cadastrar() {
+    if (nome1 == "Jogador 1") {
+
+    }
+    else {
+        jog2 = Nick.Text;
+        label_jogador2.Text = jog2;
+        btn_cadastrar.Visible = false;
+        Nick.Visible = false;
+    }
 }
